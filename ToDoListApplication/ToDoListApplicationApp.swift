2866f557429815fa9b691945a051bbf4a3bad40a
Pujan Bohora <bohorapawan@gmail.com>
2024-05-05 14:44:23 +0545

    Initial Commit

---
+++ ToDoListApplication/ToDoListApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApplicationApp.swift
+//  ToDoListApplication
+//
+//  Created by Pawan Bohora on 5/5/24.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
